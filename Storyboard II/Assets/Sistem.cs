2ae5f40d5f8512c17887d9299c480a1371d3a5dd
SalsabilaFirdausy <salsa.firdausy@gmail.com>
2021-10-09 23:03:10 +0700

    Storyboard II

---
+++ Storyboard II/Assets/Sistem.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sistem : MonoBehaviour
+{
+    public static Sistem instance;
+    //id memanggil urutan buah
+    public int ID;
+    //tempat spawn object
+    public GameObject TempatSpawn; 
+    //array koleksi buah
+    public GameObject[] KoleksiBuah;
+    
+    //akan diakses sebelum start
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ID = 0;
+        SpawnObject();
+    }
+
+    public void SpawnObject()
+    {
+        //menghancurkan benda sebelumnya dengan tag "buah"
+        GameObject BedaSebelumnya = GameObject.FindGameObjectWithTag("Buah");
+        //menghancurkan benda sebelumnya jika ada bendanya
+        if(BedaSebelumnya != null) Destroy(BedaSebelumnya);
+
+        //memunculkan benda gameoject
+        GameObject Object = Instantiate(KoleksiBuah[ID]);
+        //memasukkan gambar yang ingin dimunculkan dalam TempatSpawn
+        Object.transform.SetParent(TempatSpawn.transform, false);
+        //setting ukuran gambar
+        Object.transform.localScale = new Vector3 (2906,2906,2906);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            ChangeFruit(true);
+        }
+
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            ChangeFruit(false);
+        }
+    }
+
+    //jika tombol arrow keyboard ditekan, gambar akan berubah
+    private void ChangeFruit(bool kanan)
+    {
+        if(kanan)
+        {
+            if(ID >= KoleksiBuah.Length - 1)
+            {
+                ID = 0;
+            }
+            else
+            {
+                ID++;
+            }
+        }
+        else
+        {
+            if(ID <= 0)
+            {
+                ID = KoleksiBuah.Length - 1;
+            }
+            else
+            {
+                ID--;
+            }
+        }
+
+        SpawnObject();
+    }
+    
+}
